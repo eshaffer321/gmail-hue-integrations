2df403c8bc0a8cd7f81e0314d60155b492b44a85
Erick Shaffer <erickshaffer@Ericks-MacBook-Pro.local>
2018-10-31 18:39:32 -0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+var morgan = require('morgan');
 const app = express();
+
+app.use(morgan('combined'));
 
 app.listen(8080, () => {
     console.log("server is up and running");
+});
+
+app.get('/', function (req, res) {
+    res.send('hello, world!')
 });
